ae91b00afa579dbd83c71860f66cce1424738695
Adam281275 <schilder@aichberger.com>
2025-06-01 08:53:40 +0200

    Create Parameter.swift

---
+++ Parameter.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+struct KalkulationsParameter {
+    var aufschlagProzent: Double // z.B. 80.0 für 80%
+    var fixkosten: Double        // z.B. 15.0 EUR
+    var rabattMaxFlaeche: Double // maximaler Flächenrabatt in %
+    var rabattMaxStueck: Double  // maximaler Stückzahlrabatt in %
+}
