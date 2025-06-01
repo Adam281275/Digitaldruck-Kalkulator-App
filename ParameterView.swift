8f4e1d2f60c2e8dcc2b000b7089adc3ae5a31f43
Adam281275 <schilder@aichberger.com>
2025-06-01 08:51:50 +0200

    Create ParameterView.swift

---
+++ ParameterView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct ParameterView: View {
+    var body: some View {
+        Text("Kalkulationsparameter kommen hier hin")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct ParameterView_Previews: PreviewProvider {
+    static var previews: some View {
+        ParameterView()
+    }
+}
