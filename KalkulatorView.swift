f0dc42e59eca928aa755a16693ce3ede8add318c
Adam281275 <schilder@aichberger.com>
2025-06-01 08:51:06 +0200

    Update KalkulatorView.swift

--- KalkulatorView.swift
+++ KalkulatorView.swift
@@ -3,5 +3,13 @@ import SwiftUI
 struct KalkulatorView: View {
     var body: some View {
         Text("Kalkulator kommt hier hin")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct KalkulatorView_Previews: PreviewProvider {
+    static var previews: some View {
+        KalkulatorView()
     }
 }
