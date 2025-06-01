c550c5a61f1ce74ffee9ac9c2ea2fc649e9ade94
Adam281275 <schilder@aichberger.com>
2025-06-01 08:50:15 +0200

    Create MaterialienView.swift

---
+++ MaterialienView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct MaterialienView: View {
+    var body: some View {
+        Text("Materialverwaltung kommt hier hin")
+            .font(.title)
+            .padding()
+    }
+}
+
+struct MaterialienView_Previews: PreviewProvider {
+    static var previews: some View {
+        MaterialienView()
+    }
+}
